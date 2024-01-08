7e72dd1e10f4af51a2df53b81990e6265d9ddfde
Alifur Rahman <alifurrahman1999@gmail.com>
2024-01-08 13:46:42 +0600

    update

---
+++ biowound-nav-menu/assets/js/scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+	// Mobile Menu
+	// $('.logo a i').click(function(){
+	// 	$('.menu ul').slideToggle(1000);
+
+	// 	return false
+	// });
+
+	// Banner Carousel
+	// $('.banner_carousel').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	nav:true,
+	// 	dots:true,
+	// 	autoplay:true,
+	// });
+
+	// Mobile Menu Icon
+
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+		$(this).toggleClass('open');
+		$('body').toggleClass('show-nav');
+		return false;
+	});
+	$(".newco-header__nav").on("click", function (event) {
+		if (!$(event.target).hasClass("newco-header__nav-list")) {
+			$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
+			$('body').toggleClass('show-nav');
+		}
+		return false;
+	});
+	$('.has-children').on('click', function () {
+		if ($(window).width() < 1200) {
+			$(this).addClass('active');
+			$(this).find('.newco-header__nav-list-wrapper').addClass('active');
+		}
+		return false;
+	});
+
+	$('.back-button').on('click', function () {
+		$(this).parent().removeClass('active');
+		$(this).parent().parent().removeClass('active');
+		return false;
+	});
+});
+
+$(window).resize(function () {
+	if ($(window).width() > 1200) {
+		$('.has-children').removeClass('active');
+		$('.newco-header__nav-list-wrapper').removeClass('active');
+	}
+})
